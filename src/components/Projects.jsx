b6296a42eeb7cc8c7afca298f52470010b8bdcf0
alynguyxn <lyssbliss610@gmail.com>
2026-04-16 00:12:45 -0400

    formatting card on project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ export default function Projects() {
           <div className="project-card-featured">
             <div className="project-image-container">
               {/* Use your lily2 image as a cover for the card */}
-              <img src="/images/sitecover.png" alt="Website Documentation" className="project-image" />
+              <img src="/images/notioncover.png" alt="Website Documentation" className="project-image" />
               <div className="overlay-text">View Documentation</div>
             </div>
             <div className="project-info">
