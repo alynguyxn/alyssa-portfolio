e17d3fae750c04c8fa7fcdcbc5fc780364c37aac
alynguyxn <lyssbliss610@gmail.com>
2026-04-19 02:33:23 -0400

    formatting about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,26 @@
 function About() {
   return (
-    <section id="about"> {/* Added the ID here */}
-      <div className="section-content"> {/* Added this wrapper */}
-        <h1>about me</h1>
-        <h3>
-          Hi! My name is Alyssa and I enjoy designing and <br></br>coding websites. 
-          I'm a Junior majoring in Computer Science<br></br> at the University of Central Florida. 
-          In my free time, I <br></br> enjoy playing games, discovering new music, 
-          and spending <br></br>time with friends!
-        </h3>
+    <section id="about">
+      <div className="about-container">
+        
+        {/* Your Photo */}
+        <img 
+          src="/images/anpic.jpg" 
+          alt="Alyssa Nguyen" 
+          className="about-photo" 
+        />
+
+        {/* Your Text Content */}
+        <div className="about-text">
+          <h1>about me</h1>
+          <h3>
+            Hi! My name is Alyssa and I enjoy designing and coding websites. 
+            I'm a Junior majoring in Computer Science at the University of Central Florida. 
+            In my free time, I enjoy playing games, discovering new music, 
+            and spending time with friends!
+          </h3>
+        </div>
+
       </div>
     </section>
   );
